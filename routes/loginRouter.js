153c470d7816366056ccdfb7afbf9c6bcaad0932
Manuel Entrena <manuel.entrena@gmail.com>
2021-07-02 13:31:28 +0200

    Añadimos ENDPOINT get login

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { error, success } from "../connection/responses.js";
 import controller from "../controllers/loginController.js";
+import auth from "../middleware/verifyToken.js";
+import authUser from "../middleware/verifyUser.js";
 
 const login = express.Router();
 
@@ -10,6 +12,16 @@ login.post("/", async (req, res) => {
   try {
     const response = await controller.loginUser({ email, password });
     success({ req, res, data: response, status: 201, msg: "Logged User" });
+  } catch (info) {
+    error({ req, res, error: "error", status: 500, info });
+  }
+});
+
+login.get("/", auth, authUser, async (req, res) => {
+  const user = req.user;
+  console.log("DENTRO");
+  try {
+    success({ req, res, data: user, status: 201, msg: "Username" });
   } catch (info) {
     error({ req, res, error: "error", status: 500, info });
   }
